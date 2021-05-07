bdc99f876e8bea6b3698d385e27ddfed05130e89
Michael Muscato <>
2021-05-06 21:10:25 -0600

    Complete refactor.  Moved to computed fields and conditional rendering.  Style updates.

--- tmus-boot-camp/src/components/calculator.vue
+++ tmus-boot-camp/src/components/calculator.vue
@@ -1,15 +1,51 @@
 <template>
   <div id="calculator">
     <h1>{{ msg }}</h1>
+
     <h3>Enter two integers:</h3>
-    <input v-model.number="integerX" type="number" @keypress="onlyIntegers" placeholder="Enter an integer">
-    <input v-model.number="integerY" type="number" @keypress="onlyIntegers" placeholder="Enter another integer">
-    <button v-on:click="doMath">Let's do some math!</button>
-    <p>{{ sumOfTwoInts }}</p>
-    <p>{{ diffOfTwoInts }}</p>
-    <p>{{ prodOfTwoInts }}</p>
-    <p>{{ quotOfTwoInts }}</p>
-    <p>{{ modOfTwoInts }}</p>
+    <div id="inputForm">
+      <input v-model.lazy.number="integerX" type="number" @keypress="onlyIntegers" placeholder="Enter an integer">
+      <input v-model.number="integerY" type="number" @keypress="onlyIntegers" placeholder="Enter another integer">
+    </div>
+
+    <div id="results">
+      <div>
+        <span v-if="integerY && !integerX" class="left">Please enter the first integer.</span>
+        <span v-else-if="integerX && integerY" class="left">The sum of {{integerX}} + {{integerY}} is:</span>
+        <span v-else-if="integerX" class="left">The sum of {{integerX}} and...</span>
+          <span v-if="integerX && integerY" class="right">{{ sumOfTwoInts }}</span>
+          <span v-else class="right"></span>
+      </div>
+
+      <div>
+        <span v-if="integerX && integerY" class="left">The difference of {{integerX}} - {{integerY}} is:</span>
+        <span v-else-if="integerX" class="left">The difference of {{integerX}} minus...</span>
+          <span v-if="integerX && integerY" class="right">{{ diffOfTwoInts }}</span>
+          <span v-else class="right"></span>
+      </div>
+
+      <div>
+        <span v-if="integerX && integerY" class="left">The product of {{integerX}} X {{integerY}} is:</span>
+        <span v-else-if="integerX" class="left">The product of {{integerX}} times...</span>
+          <span v-if="integerX && integerY" class="right">{{ prodOfTwoInts }}</span>
+          <span v-else class="right"></span>
+      </div>
+
+      <div>
+        <span v-if="integerX && integerY" class="left">The quotient of {{integerX}} / {{integerY}} is:</span>
+        <span v-else-if="integerX" class="left">The quotient of {{integerX}} divided by...</span>
+          <span v-if="integerX && integerY" class="right">{{ quotOfTwoInts }}</span>
+          <span v-else class="right"></span>
+      </div>
+
+      <div>
+        <span v-if="integerX && integerY" class="left">The modulo (remainder) of {{integerX}} % {{integerY}} is:</span>
+        <span v-else-if="integerX" class="left">The modulo (remainder) of {{integerX}} divided by...</span>
+          <span v-if="integerX && integerY" class="right">{{ modOfTwoInts }}</span>
+          <span v-else class="right"></span>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -21,29 +57,30 @@ export default {
   },
   data() {
     return {
-      sumOfTwoInts: '',
-      diffOfTwoInts: '',
-      prodOfTwoInts: '',
-      quotOfTwoInts: '',
-      modOfTwoInts: '',
       integerX: '',
       integerY: ''
     }
   },
-  methods: {
-    doMath() {
-      //Calc sum
-      this.getSum(this.integerX, this.integerY);
-      //Calc difference
-      this.getDifference(this.integerX, this.integerY);
-      //Calc product
-      this.getProduct(this.integerX, this.integerY);
-      //Calc quotient
-      this.getQuotient(this.integerX, this.integerY);
-      //Calc modulus
-      this.getModulus(this.integerX, this.integerY);
-    },
 
+  computed: {
+    sumOfTwoInts() {  //Addition
+      return this.integerX + this.integerY;
+    },
+    diffOfTwoInts() {  //Subraction
+      return this.integerX - this.integerY;
+    },
+    prodOfTwoInts() {  //Multiplication
+      return this.integerX * this.integerY;
+    },
+    quotOfTwoInts() {  //Division
+      return Math.round(this.integerX / this.integerY * 100) / 100
+    },
+    modOfTwoInts() {  //Modulo
+      return this.integerX % this.integerY;
+    }
+  },
+
+  methods: {
     onlyIntegers($event) {
       //if keyCode returns, use it otherwise use the char code.  For x-browser support.
       let keyCode = ($event.keyCode ? $event.keyCode : $event.which);
@@ -52,29 +89,7 @@ export default {
       if (keyCode == 46) {
         $event.preventDefault();
       }
-    },
-
-    //Math functions broken out for readability.
-    getSum(intX, intY) {
-      var sum = intX + intY;
-      this.sumOfTwoInts = "The sum of " + intX + " and " + intY + " is: " + sum;
-    },
-    getDifference(intX, intY) {
-      var difference = intX - intY;
-      this.diffOfTwoInts = "The difference of " + intX + " minus " + intY + " is: " + difference;
-    },
-    getProduct(intX, intY) {
-      var product = intX * intY;
-      this.prodOfTwoInts = "The product of " + intX + " times " + intY + " is: " + product;
-    },
-    getQuotient(intX, intY) {
-      var quotient = intX / intY;
-      this.quotOfTwoInts = "The quotient of " + intX + " divided by " + intY + " is: " + quotient;
-    },
-    getModulus(intX, intY) {
-      var modulus = intX % intY;
-      this.modOfTwoInts = "The modulus (remainder) of " + intX + " divided by " + intY + " is: " + modulus;
     }
   }
 }
-</script>\
+</script>
